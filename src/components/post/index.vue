6c1876404e6b9da12cb9e21ffd08d76e7c23d88c
1622583480 <1622583480@qq.com>
2025-07-07 21:19:06 +0800

    fix: update right image

--- src/components/post/index.vue
+++ src/components/post/index.vue
@@ -5,14 +5,14 @@
                 <PostHeader v-if="user" :user="user" :post="post" />
                 <h3 class="title mb-2 mt-3 font-bold">{{ post.title }}</h3>
                 <div class="flex">
-                    <div class="flex flex-col flex-1">
+                    <div class="flex flex-col flex-1 mr-2">
                         <n-performant-ellipsis class="desu" line-clamp="3" :tooltip="false">
                             {{ post.introduction }}
                         </n-performant-ellipsis>
                         <Tag class="mt-auto" :post="post" />
                     </div>
-                    <div class="relative header-img-view ml-auto pl-6" v-if="post.headerImage">
-                        <n-image class="absolute header-img " object-fit="cover" width="180" :src="post.headerImage"
+                    <div class="relative header-img-view ml-auto" v-if="post.headerImage">
+                        <n-image class="absolute header-img " object-fit="cover" :width="imageWidth" :src="post.headerImage"
                             preview-disabled lazy />
                     </div>
                 </div>
@@ -25,6 +25,11 @@
 <script setup lang="ts">
 import PostFooter from './postFooter.vue';
 import PostHeader from './postHeader.vue';
+import { useMediaQuery } from '@vueuse/core';
+
+const isMobile = useMediaQuery('(max-width: 600px)');
+const imageWidth = computed(() => isMobile.value ? 108 : 180);
+
 const { post, user } = defineProps<{
     post: Post
     user?: User
@@ -56,6 +61,16 @@ const { post, user } = defineProps<{
     min-height: 90px;
     height: 100%;
 }
+
+@media (max-width: 600px) {
+    .header-img-view {
+        min-width: 108px;
+        width: 108px;
+
+        height: 72px;
+    }
+}
+
 
 .header-img {
     border-radius: 8px;
